44de31bf93845131e13c2ee33a8398a48823967d
schiale <santiagochiale@gmail.com>
2019-08-22 00:05:35 -0300

    Se agrego la escritura sobre archivo configuaracion.json dentro de la carpeta data, recibe comando para entrar en modo AP desde la aplicación, lee los parametros ingresados en la ventana de configuracion, los guarda en el archivo configuracion y cada vez que se acciona el interruptor para accionar el actuador lee ese archivo y trae los valores para mostrarlos en el puerto serie

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,11 @@
 #include <ArduinoJson.h>
 
 //define your default values here, if there are different values in config.json, they are overwritten.
-char mqtt_server[40];
-char mqtt_port[6] = "8080";
-char blynk_token[34] = "YOUR_BLYNK_TOKEN";
-
-//flag for saving data
-bool shouldSaveConfig = false;
+char parametro1[40];
+char parametro2[40];
+char parametro3[40];
+char parametro4[40];
+char parametro5[40];
 
 
 //#define DHTTYPE DHT11
@@ -46,6 +45,7 @@ float temperatura = 0;
 float humedad = 0;
 float vdd = 0;
 int beacon = 0;
+boolean SPIFFSinit = false;
 
 //******************************
 //****DECLARACION FUNCIONES*****
@@ -57,6 +57,7 @@ void reconnect();                                               //funcion para r
 void sensate(); //funcion que sensa la temperatura y humedad y la muestra por el puerto serial
 void configWiFi();
 void saveConfigCallback ();
+void readDataSPIFFS();
 
 //*****************************
 
@@ -72,6 +73,10 @@ void setup()
   client.setServer(mqttServer, mqttPort); //seteo del cliente mqtt
   client.setCallback(callback);           //listener de mensajes. cuando llegue un mensaje se ejecutara la funcion que le pasamos entre parentesis, en este caso callback
   digitalWrite(LED_BUILTIN,HIGH);
+
+  if (SPIFFS.begin()) {
+    SPIFFSinit = true;
+  }
 }
 
 void loop()
@@ -138,10 +143,13 @@ void setupWifi()
 
   WiFiManager wifiManager;
 
-  wifiManager.autoConnect("AutoConnectAP");
-
-  //if you get here you have connected to the WiFi
-  Serial.println("Conectado. Usando los ultimos parametros guardados)");
+  if (!wifiManager.autoConnect("AutoConnectAP")) {
+    configWiFi();
+    Serial.println("Entrando a config WiFi");
+  }else{
+    //if you get here you have connected to the WiFi
+    Serial.println("Conectado. Usando los ultimos parametros guardados)");
+  }
 
 }
 
@@ -172,6 +180,7 @@ void callback(char *topic, byte *payload, unsigned int length)
   if (topico=="act3" && incoming=="on") {
     Serial.println("Actuador on");
     digitalWrite(LED_BUILTIN, LOW);
+    readDataSPIFFS();
   }
   if (topico=="act3" && incoming=="off") {
     Serial.println("Actuador off");
@@ -270,17 +279,11 @@ sensors_event_t event;
  */
 }
 
-//callback notifying us of the need to save config
-void saveConfigCallback () {
-  Serial.println("Should save config");
-  shouldSaveConfig = true;
-}
-
 void configWiFi(){
-  /*
+
   WiFiManager wifiManager;
 
-  if (!wifiManager.startConfigPortal("OnDemandAP")) {
+  if (!wifiManager.startConfigPortal("ESP8266WiFi") && SPIFFSinit) {
     Serial.println("failed to connect and hit timeout");
     delay(3000);
     //reset and try again, or maybe put it to deep sleep
@@ -290,20 +293,18 @@ void configWiFi(){
 
   //if you get here you have connected to the WiFi
   Serial.println("connected...yeey :)");
-  */
 
-  //clean FS, for testing
-  //SPIFFS.format();
+}
 
-  //read configuration from FS json
+void readDataSPIFFS(){
   Serial.println("mounting FS...");
 
-  if (SPIFFS.begin()) {
+  if (SPIFFSinit) {
     Serial.println("mounted file system");
-    if (SPIFFS.exists("/config.json")) {
+    if (SPIFFS.exists("/configuracion.json")) {
       //file exists, reading and loading
       Serial.println("reading config file");
-      File configFile = SPIFFS.open("/config.json", "r");
+      File configFile = SPIFFS.open("/configuracion.json", "r");
       if (configFile) {
         Serial.println("opened config file");
         size_t size = configFile.size();
@@ -317,10 +318,17 @@ void configWiFi(){
         if (json.success()) {
           Serial.println("\nparsed json");
 
-          strcpy(mqtt_server, json["mqtt_server"]);
-          strcpy(mqtt_port, json["mqtt_port"]);
-          strcpy(blynk_token, json["blynk_token"]);
+          strcpy(parametro1, json["parametro1"]);
+          strcpy(parametro2, json["parametro2"]);
+          strcpy(parametro3, json["parametro3"]);
+          strcpy(parametro4, json["parametro4"]);
+          strcpy(parametro5, json["parametro5"]);
 
+          Serial.println(parametro1);
+          Serial.println(parametro2);
+          Serial.println(parametro3);
+          Serial.println(parametro4);
+          Serial.println(parametro5);
         } else {
           Serial.println("failed to load json config");
         }
@@ -331,84 +339,4 @@ void configWiFi(){
     Serial.println("failed to mount FS");
   }
   //end read
-
-
-
-  // The extra parameters to be configured (can be either global or just in the setup)
-  // After connecting, parameter.getValue() will get you the configured value
-  // id/name placeholder/prompt default length
-  WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 40);
-  WiFiManagerParameter custom_mqtt_port("port", "mqtt port", mqtt_port, 6);
-  WiFiManagerParameter custom_blynk_token("blynk", "blynk token", blynk_token, 32);
-
-  //WiFiManager
-  //Local intialization. Once its business is done, there is no need to keep it around
-  WiFiManager wifiManager;
-
-  //set config save notify callback
-  wifiManager.setSaveConfigCallback(saveConfigCallback);
-
-  //set static ip
-  wifiManager.setSTAStaticIPConfig(IPAddress(10,0,1,99), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
-
-  //add all your parameters here
-  wifiManager.addParameter(&custom_mqtt_server);
-  wifiManager.addParameter(&custom_mqtt_port);
-  wifiManager.addParameter(&custom_blynk_token);
-
-  //reset settings - for testing
-  //wifiManager.resetSettings();
-
-  //set minimu quality of signal so it ignores AP's under that quality
-  //defaults to 8%
-  //wifiManager.setMinimumSignalQuality();
-
-  //sets timeout until configuration portal gets turned off
-  //useful to make it all retry or go to sleep
-  //in seconds
-  //wifiManager.setTimeout(120);
-
-  //fetches ssid and pass and tries to connect
-  //if it does not connect it starts an access point with the specified name
-  //here  "AutoConnectAP"
-  //and goes into a blocking loop awaiting configuration
-  if (!wifiManager.autoConnect("AutoConnectAP", "password")) {
-    Serial.println("failed to connect and hit timeout");
-    delay(3000);
-    //reset and try again, or maybe put it to deep sleep
-    ESP.reset();
-    delay(5000);
-  }
-
-  //if you get here you have connected to the WiFi
-  Serial.println("connected...yeey :)");
-
-  //read updated parameters
-  strcpy(mqtt_server, custom_mqtt_server.getValue());
-  strcpy(mqtt_port, custom_mqtt_port.getValue());
-  strcpy(blynk_token, custom_blynk_token.getValue());
-
-  //save the custom parameters to FS
-  if (shouldSaveConfig) {
-    Serial.println("saving config");
-    DynamicJsonBuffer jsonBuffer;
-    JsonObject& json = jsonBuffer.createObject();
-    json["mqtt_server"] = mqtt_server;
-    json["mqtt_port"] = mqtt_port;
-    json["blynk_token"] = blynk_token;
-
-    File configFile = SPIFFS.open("/config.json", "w");
-    if (!configFile) {
-      Serial.println("failed to open config file for writing");
-    }
-
-    json.printTo(Serial);
-    json.printTo(configFile);
-    configFile.close();
-    //end save
-  }
-
-  Serial.println("local ip");
-  Serial.println(WiFi.localIP());
-
 }
